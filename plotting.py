f77b651e292a2a1302b8ac9b3a02e47aa7a97c40
Philip <philipschubert00@gmail.com>
2023-05-31 13:19:40 +0200

    changed to path

--- plotting.py
+++ plotting.py
@@ -1,11 +1,15 @@
 from main import *
+from pathlib import Path
 
 save_path = 'C:/Users/herzu/Downloads'
-gedi_path = 'C:/Users/herzu/Downloads/gedi_data/GEDI_clipped.gpkg'
+gedi_path = 'C:/Users/herzu/Downloads/GEDI_clipped.gpkg'
 
-create_boxplots(gedi_path, save_path)
+save_path = Path(r'{}'.format(save_path))
+gedi_path = Path(r'{}'.format(gedi_path))
+
+#create_boxplots(gedi_path, save_path)
 plot_correlation_matrix(gedi_path, save_path)
-create_correlation_plots(gedi_path, save_path)
+#create_correlation_plots(gedi_path, save_path)
 
 
 """"
@@ -17,3 +21,16 @@ GEDI_table = GEDI.describe(percentiles=[.1, .2, .3, .4, .5, .6, .7, .8, .9])
 GEDI_table.to_csv('C:/Users/herzu/Downloads/GEDI_Deskriptive_Statistik.csv', float_format='%.5f')
 
 """
+"""""
+GEDI = gp.read_file(gedi_path)
+
+
+
+import pandas as pd
+from IPython.display import display
+GEDI_table = GEDI.describe(percentiles=[.1, .2, .3, .4, .5, .6, .7, .8, .9])
+# Assuming you have the GEDI_table DataFrame
+pd.set_option('display.max_columns', None)
+display(GEDI_table)
+
+"""""
