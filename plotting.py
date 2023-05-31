6b93b990d316ff21ba70ef0b6b9786166c67b594
Philip <philipschubert00@gmail.com>
2023-05-31 13:52:33 +0200

    with marco function

--- plotting.py
+++ plotting.py
@@ -1,6 +1,5 @@
 from main import *
 from pathlib import Path
-import sys
 
 path = r'D:\Studium\GEO 411\Daten\geo411_gedi\Daten'
 gedi_data = Path(r'GEDI_clipped.gpkg')
@@ -8,10 +7,20 @@ gedi_data = Path(r'GEDI_clipped.gpkg')
 save_path = Path(r'{}'.format(path))
 #gedi_path = Path(r'{}\{}'.format(path, gedi_data))
 gedi_path = save_path / gedi_data
+l2a_gpkg = 'C:/Users/herzu/Downloads/L2A.gpkg'
+l2b_gpkg = 'C:/Users/herzu/Downloads/L2B.gpkg'
+time_range = ['2019-04-28', '2022-04-29']
 
-create_boxplots(gedi_path, save_path)
-plot_correlation_matrix(gedi_path, save_path)
+save_path = Path(r'{}'.format(save_path))
+gedi_path = Path(r'{}'.format(gedi_path))
+path_l2a = Path(r'{}'.format(l2a_gpkg))
+path_l2b = Path(r'{}'.format(l2b_gpkg))
+
+#load_gedi_gpkg(path_l2a, path_l2b, time_range, save_path)
+#create_boxplots(gedi_path, save_path)
+#plot_correlation_matrix(gedi_path, save_path)
 #create_correlation_plots(gedi_path, save_path)
+
 
 
 """"
