5104aa5cceed19d547dcce413375009938b03073
Philip <philipschubert00@gmail.com>
2023-05-31 13:52:39 +0200

    with marco function

--- main.py
+++ main.py
@@ -1,12 +1,30 @@
 import geopandas as gp
+import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
 from scipy import stats
-from pathlib import Path
+
+
+def load_gedi_gpkg(path_l2a, path_l2b, time_range, save_path):
+
+    gedi_data_l2a = gp.read_file(path_l2a)
+    gedi_data_l2a = gedi_data_l2a.loc[:, ['Relative Height bin98 (cm)', 'geometry']]
+    gedi_data_l2b = gp.read_file(path_l2b)
+
+    gedi_data = gedi_data_l2b.merge(gedi_data_l2a, how='inner', on='geometry')
+    gedi_data['time'] = pd.to_datetime(gedi_data['Acquisition Time'])
+    gedi_data = gedi_data.set_index('time')
+    if time_range is not None:
+        assert len(time_range) == 2, "time_range must be a list of two elements"
+        gedi_data = gedi_data.sort_index().loc[f'{time_range[0]}':f'{time_range[1]}']
+
+    gedi_data.to_file(save_path / 'GEDI_clipped.gpkg', driver="GPKG")
+    return gedi_data
 
 
 def create_boxplots(gedi, save_filepath):
-    variable_names = ['Canopy_cover', 'Height_98', 'Plant_area', 'Foliage_height']
+    variable_names = ['Total Canopy Cover', 'Relative Height bin98 (cm)', 'Total Plant Area Index',
+                      'Foliage Height Diversity Index']
     gedi = gp.read_file(gedi)
 
     fig, axs = plt.subplots(nrows=2, ncols=2, figsize=(10, 8))
@@ -19,24 +37,25 @@ def create_boxplots(gedi, save_filepath):
         data = gedi[variable_name].dropna().values
         ax.boxplot(data)
         # Manuelle Änderung der Überschrift
-        if variable_name == 'Canopy_cover':
+        if variable_name == 'Total Canopy Cover':
             ax.set_title('Total Canopy Cover')
-        elif variable_name == 'Height_98':
+        elif variable_name == 'Relative Height bin98 (cm)':
             ax.set_title('Relative Height bin98 (cm)')
-        elif variable_name == 'Plant_area':
+        elif variable_name == 'Total Plant Area Index':
             ax.set_title('Total Plant Area Index')
-        elif variable_name == 'Foliage_height':
+        elif variable_name == 'Foliage Height Diversity Index':
             ax.set_title('Foliage Height Diversity Index')
 
     # Save the figure
-    plt.savefig(save_filepath / r'boxplot.png')
+    plt.savefig(save_filepath / 'boxplot.png')
 
     # Show the plot
     plt.show()
 
 
 def plot_correlation_matrix(gedi, save_filepath):
-    variable_names = ['Canopy_cover', 'Height_98', 'Plant_area', 'Foliage_height']
+    variable_names = ['Total Canopy Cover', 'Relative Height bin98 (cm)', 'Total Plant Area Index',
+                       'Foliage Height Diversity Index']
     variable_labels = ['Total Canopy Cover', 'Relative Height bin98 (cm)', 'Total Plant Area Index',
                        'Foliage Height Diversity Index']
     gedi = gp.read_file(gedi)
@@ -56,13 +75,14 @@ def plot_correlation_matrix(gedi, save_filepath):
     ax.set_title("Correlation Matrix")
     # Save the plot
     plt.tight_layout()
-    plt.savefig(save_filepath / r'correlationamtrix.png')
+    plt.savefig(save_filepath / 'correlationamtrix.png')
     # Show the plot
     plt.show()
 
 
 def create_correlation_plots(gedi, save_filepath):
-    variable_names = ['Canopy_cover', 'Height_98', 'Plant_area', 'Foliage_height']
+    variable_names = ['Total Canopy Cover', 'Relative Height bin98 (cm)', 'Total Plant Area Index',
+                       'Foliage Height Diversity Index']
     variable_labels = ['Total Canopy Cover', 'Relative Height bin98 (cm)', 'Total Plant Area Index',
                        'Foliage Height Diversity Index']
     # Read the GEDI geopackage into a GeoDataFrame
@@ -95,6 +115,6 @@ def create_correlation_plots(gedi, save_filepath):
         g.axes[i, 0].set_ylabel(label)
         g.axes[-1, i].set_xlabel(label)
 
-        plt.savefig(save_filepath / r'correlationplot.png')
+        plt.savefig(save_filepath / 'correlationplot.png')
     # Show the plot
     plt.show()
