856cd01cafc4c791d1f1bd1c646b7ffb936f0407
Niklas-Schm <Niklas.Schmitt98@web.de>
2023-07-06 14:51:26 +0200

    load eo and gedi in xr with selection

--- pages/6_GEDI_EO_Maps.py
+++ pages/6_GEDI_EO_Maps.py
@@ -1,5 +1,6 @@
 from main import *
 from pathlib import Path
+import streamlit as st
 
 # Öffnen der NetCDF-Datei
 dataset = xr.open_dataset(r'D:\Studium\GEO 411\Daten\geo411_gedi\Daten\dataset.nc')
@@ -14,4 +15,21 @@ parameter_names = ['Total Canopy Cover', 'Relative Height bin98 (cm)', 'Total Pl
 
 eo_gedi_data = add_gedi_to_xr(dataset, gedi_gdf, parameter_names, ('-20', '20'))
 
-print(eo_gedi_data)
+# Extrahieren der Variablen vv, vh, ndvi und GEDI-Parameter
+vv = eo_gedi_data['vv']
+vh = eo_gedi_data['vh']
+ndvi = eo_gedi_data['ndvi']
+total_canopy_cover = eo_gedi_data['Total Canopy Cover']
+relative_height = eo_gedi_data['Relative Height bin98 (cm)']
+total_plant_area_index = eo_gedi_data['Total Plant Area Index']
+foliage_height_diversity_index = eo_gedi_data['Foliage Height Diversity Index']
+
+# Parameter selection box
+parameter_names = ['Total Canopy Cover', 'Relative Height bin98 (cm)', 'Total Plant Area Index',
+                   'Foliage Height Diversity Index']
+selected_parameter = st.selectbox('Parameter', parameter_names)
+selected_eo_gedi_data = eo_gedi_data['{}'.format(selected_parameter)]
+
+
+st.write('streamlit: ', selected_eo_gedi_data)
+print('prnt: ', selected_eo_gedi_data)
